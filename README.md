2801935cfd3c133a0036fa154cd7509b5bb828c1
Niko Lewis <nikolewis01@gmail.com>
2018-01-08 14:52:57 -0500

    Project 1

---
+++ README.md
@@ -0,0 +1,93 @@
+### Title: BOMB
+
+# Bomb Diffusing Logic Game
+
+### Rules:
+
+1. Read all hints and remember them.</li>
+2. Use logic to dechiper hints before deciding which wires to cut before the timer runs out.
+3. Don't Die.
+
+### Minimum Viable Product (MVP):
+
+* Basic one-player, 1 level game with a how to play/intro page ( 2 views total)
+* Player wins when bomb is diffused or die's if wron wire is cut
+
+### Reach Goals:
+
+* Multiple additional levels (2 or 3 max) with increasing difficulty levels
+* diffetent styles of diffusing ( key pads, wire cutting, ???)
+* Welcome page
+  -animations
+* advanced storybuilding
+  -timer
+
+### Potential Blockers:
+
+* I'm using this game to practice a collection of skills I realized I'd like to improve from past assignments,
+  so applying the concepts smoothly while building something unique from sratch is somewhat daunting.
+
+### Timeline:
+
+**Tuesday:**
+
+* Brainstorm ideas ✅
+* Create a repository ✅
+* Submit Proposal ✅
+
+**Wednesday:**
+
+* Determine specifications ✅
+* Wireframe ✅
+* Flowchart of MVP gameplay ✅
+* code basic level 1 functionality ✅
+* playtesting ✅
+
+**Thursday:**
+
+* Code Intro view✅
+* Add markup based Readme ✅
+* Debug ✅
+* Psuedo code making riddles and hints dynamic ✅
+
+**Friday**
+
+* Fix merge conflicts w/ vim ✅
+* Code dynamic riddles algorithm ✅
+* Code checking that win conditions is completely met after each click ✅
+* Improve bomb wiring design add toggle to cut wires ✅
+* Level one win and loss modals ✅
+* Create wire cutter cursor when hovering over bomb wiring ✅
+
+**Saturday**
+
+* Level one win and loss modals ✅
+* Level one modals styling ✅
+* Level one view styling ✅
+
+**Sunday**
+
+* Intro view styling ✅
+
+**Monday**
+
+* MVP reached✅
+* Create Power Point
+* Finalize Readme
+* Reach goals:
+
+  * landing view creation
+  * Bonus: start coding other levels ???
+  * Bonus Bonus: upgrade modals to accomodate new levels???
+
+**Tuesday**
+
+* polishing and refactoring
+
+* Final Presentation
+  ### Tech Stack:
+* HTML5
+* CSS3
+* JavaScript ES6
+* jQuery 3.2
+* Flexbox
---
+++ scripts/level1.js
@@ -0,0 +1,208 @@
+//making sure my js file is connected
+console.log('sup from the js file');
+
+//using jQuery ready() method which occurs earlier thant he DOM window.onload method would https://www.w3schools.com/jquery/event_ready.asp
+$(document).ready(function(argument) {
+  dynamicRiddle(riddles);
+});
+
+var riddles = [
+  {
+    hints: {
+      1: `Cut all odd wires`,
+      2: `Don't cut red wires even if another hint says that you should.`,
+      3: `When hint 1 said "odd" it meant "even".`,
+    },
+    wires: {
+      1: false,
+      2: true,
+      3: false,
+    },
+    checked: 0,
+    goal: 1,
+  },
+  {
+    hints: {
+      1: `Don't cut Yellow wires even if another hint says you should`,
+      2: `Green wires are actually Yellow`,
+      3: `Cut all wires".`,
+    },
+    wires: {
+      1: true,
+      2: false,
+      3: true,
+    },
+    checked: 0,
+    goal: 2,
+  },
+  {
+    hints: {
+      1: `don't cut wire 1`,
+      2: ` cut all odd wires even if another rule says you shouldn't`,
+      3: `wire 2 is actually wire 3 and wire 3 is actually wire 2`,
+    },
+    wires: {
+      1: true,
+      2: true,
+      3: false,
+    },
+    checked: 0,
+    goal: 2,
+  },
+  {
+    hints: {
+      1: `don't cut wire 3`,
+      2: `cut all green wires even if another rule says you shouldn't`,
+      3: `all wires are green`,
+    },
+    wires: {
+      1: true,
+      2: true,
+      3: true,
+    },
+    checked: 0,
+    goal: 3,
+  },
+  {
+    hints: {
+      1: `don't cut wire 3 even if another rule says you should`,
+      2: `cut all wires next to red wires`,
+      3: `wire 2 is actually red`,
+    },
+    wires: {
+      1: true,
+      2: true,
+      3: false,
+    },
+    checked: 0,
+    goal: 2,
+  },
+];
+
+var whichIndex;
+
+function dynamicRiddle(argument) {
+  var randomRiddle = Math.floor(Math.random() * 5);
+  whichIndex = randomRiddle;
+  console.log('which Index? ', whichIndex);
+
+  $('.hint1-text').text(`${riddles[randomRiddle]['hints'][1]}`);
+  $('.hint2-text').text(`${riddles[randomRiddle]['hints'][2]}`);
+  $('.hint3-text').text(`${riddles[randomRiddle]['hints'][3]}`);
+
+  // working example of how jQuery data()
+  // $('.wire1').data('check', true)
+
+  // $('.wire1').data(`trueOrFalse`, `${riddles[randomRiddle]['wires'][1]}`);
+  // $('.wire2').data(`trueOrFalse`, `${riddles[randomRiddle]['wires'][2]}`);
+  // $('.wire2').data(`trueOrFalse`, `${riddles[randomRiddle]['wires'][3]}`);
+
+  $('.wire1').attr(`title`, `${riddles[randomRiddle]['wires'][1]}`);
+  $('.wire2').attr(`title`, `${riddles[randomRiddle]['wires'][2]}`);
+  $('.wire3').attr(`title`, `${riddles[randomRiddle]['wires'][3]}`);
+
+  console.log($('.wire1').attr('title'));
+  console.log($('.wire2').attr('title'));
+  console.log($('.wire3').attr('title'));
+}
+
+// failed attempt to use bind so this would refer to element, look into this
+// looked up event properties to figure out one I could access, went with "title", will refactor to use jQuery data() maybe... but can't figure out how to retrieve it.
+
+// $('.container1').bind('click', function(event) {
+//   // if (event.currentTarget.value === true) {
+//   //   alert(`Good work! Bomb diffused.`);
+//   // } else {
+//   //   alert(`Wrong wire dummy, now you're dead.`);
+//   // }
+
+//   console.log(
+//     `${this.currentTarget}
+//     } !`
+//   );
+// });
+
+//placing an event listener to know when a wire is clicked
+//next step make it so that a winning or losing  module covers the screen with a restart or adnvace button
+
+$('.container1').click(function(event) {
+  var storeEvent = event;
+  // trying drakes dataset suggestion
+  // console.log(` dataset? ${event.target.dataset}`);
+  console.log('event.target', event.target);
+  console.log(`.wire1 .color1)`, $('.color1')[0]);
+  if (event.target === $('.color1')[0]) {
+    $('.color1').css(`background`, `url('../images/redwire-cut.png')`);
+  } else if (event.target === $('.color2')[0]) {
+    $('.color2').css(`background`, `url('../images/greenwire-cut.png')`);
+  } else if (event.target === $('.color3')[0]) {
+    $('.color3').css(`background`, `url('../images/bluewire-cut.png')`);
+  }
+
+  setTimeout(function() {
+    // console.log('hi from setTimeout', storeEvent);
+    if (storeEvent.target.title == 'true') {
+      riddles[whichIndex].checked += 1;
+
+      if (riddles[whichIndex].checked === riddles[whichIndex].goal) {
+        $('.modal-gif').css(
+          `background`,
+          `url('https://media.giphy.com/media/eBqUy3ik6P2vK/giphy.gif') no-repeat center center`
+        );
+
+        $('.modal-text').text(`Winner!!!`);
+
+        $('.modal-container').toggle(function(argument) {
+          //this works but throws error here
+          $('.modal-container').css('display');
+        });
+
+        $('.restart').click(function(argument) {
+          location.href = './level1.html';
+        });
+        $('.quit').click(function(argument) {
+          location.href = '../index.html';
+        });
+        // alert(`Good work! Bomb diffused.`);
+      }
+    } else if (storeEvent.target.title == 'false') {
+      $('.modal-container').toggle(function(argument) {
+        this.css('display');
+      });
+
+      $('.restart').click(function(argument) {
+        location.href = './level1.html';
+      });
+      $('.quit').click(function(argument) {
+        location.href = '../index.html';
+      });
+      // alert(`Wrong wire dummy, now you're dead.`);
+    }
+
+    console.log(` t or f? ${event.target.title}`);
+  }, 900);
+});
+
+$('.hint1-title').mouseenter(function() {
+  $('.hint1-text').css('visibility', 'visible');
+});
+
+$('.hint1-title').mouseout(function() {
+  $('.hint1-text').css('visibility', 'hidden');
+});
+
+$('.hint2-title').mouseenter(function() {
+  $('.hint2-text').css('visibility', 'visible');
+});
+
+$('.hint2-title').mouseout(function() {
+  $('.hint2-text').css('visibility', 'hidden');
+});
+
+$('.hint3-title').mouseenter(function() {
+  $('.hint3-text').css('visibility', 'visible');
+});
+
+$('.hint3-title').mouseout(function() {
+  $('.hint3-text').css('visibility', 'hidden');
+});
